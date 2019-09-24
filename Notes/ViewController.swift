9251aee988fcac687f7cbb312a1220763b449944
Ajwad <Ajwad>
2019-09-24 09:47:27 +0100

    Hamburger Menu Complete

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    
+    @IBOutlet var nameLable: UILabel!
+    @IBOutlet var menuTV: UITableView!
+    @IBOutlet var menuLeadingConstraint: NSLayoutConstraint!
+    
+     var menuItems :[String] = ["Log In","About"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    //MARK: - ACTIONS
+    @IBAction func menuButtonPressed(_ sender: UIButton) {
+        showAndHideMenu()
+    }
+    
+    
+    //MARK: - FUNCTIONS
+    func showAndHideMenu() {
+        print("HomeVC - showAndHideMenu")
+        if menuLeadingConstraint.constant == -240 {
+            menuLeadingConstraint.constant = 0
+        } else {
+            menuLeadingConstraint.constant = -240
+        }
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    //MARK: - TABLE VIEW FUNCTIONS (SIDE BAR MENU)
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return menuItems.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+           cell.textLabel?.text = menuItems[indexPath.row]
+           return cell
+       }
+    
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        showAndHideMenu()
+    }
+    
+    
 
 }
 
