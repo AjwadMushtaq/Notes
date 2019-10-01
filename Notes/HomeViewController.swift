d36fdf36edaa03f598acf9b6faeb187ad394a6d3
Ajwad <Ajwad>
2019-10-01 20:44:36 +0100

    HomeVc - user obje
    ct creaated
    
    Auth listener implement ( hamburger menu updated accordingly)

--- Notes/HomeViewController.swift
+++ Notes/HomeViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
+    
     
     @IBOutlet var nameLable: UILabel!
     @IBOutlet var menuTV: UITableView!
     @IBOutlet var menuLeadingConstraint: NSLayoutConstraint!
     
-     var menuItems : [String] = ["Log In","About"]
+    
+    var currentUser : User?
+    var authListener : AuthStateDidChangeListenerHandle?
+       
+    
+    
+    var menuItems : [String] = ["Log In","About" , "Log Out"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if authListener == nil {
+            authListener = Auth.auth().addStateDidChangeListener { (auth, user) in
+                self.currentUser = user
+                if let user = user {
+                    print("HomeVC - user signed in: \(user.uid)")
+                    
+                    
+                    self.menuItems = ["About" ,"Notes" ,"Log Out"]
+                    //self.menuItems = ["Log out" , "About" , "My profile" , "Post box","Support"]
+                   
+                } else {
+                    print("HomeVC - no user!")
+                    self.menuItems = ["Log in", "About"]
+                }
+                self.menuTV.reloadData()
+            }
+        }
+        
+        
     }
     
     
@@ -29,8 +56,8 @@ class HomeViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
     }
     
     @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
-           showAndHideMenu()
-       }
+        showAndHideMenu()
+    }
     
     //MARK: - FUNCTIONS
     func showAndHideMenu() {
@@ -47,21 +74,32 @@ class HomeViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
     
     func logIn() {
         //if Auth.auth().currentUser == nil {
-            performSegue(withIdentifier: "toSignUp", sender: self)
+        performSegue(withIdentifier: "toSignUp", sender: self)
         //}
+    }
+    
+    func logOut() {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            print("HomeVC - Succesfully Signed Out")
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
     }
     
     
     //MARK: - TABLE VIEW FUNCTIONS (SIDE BAR MENU)
-       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return menuItems.count
-       }
-       
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-           cell.textLabel?.text = menuItems[indexPath.row]
-           return cell
-       }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        return cell
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
@@ -70,15 +108,17 @@ class HomeViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
             logIn()
         case "About":
             break
+        case "Log Out":
+            logOut()
         default:
             break
         }
         showAndHideMenu()
     }
     
-   
     
     
-
+    
+    
 }
 
--- Notes/LogInViewController.swift
+++ Notes/LogInViewController.swift
@@ -17,7 +17,7 @@ enum ActionType {
 
 
 class LogInViewController: UIViewController {
-
+    
     @IBOutlet var headingDetaiLable: UILabel!
     @IBOutlet var emailField: UITextField!
     @IBOutlet var passwordField: UITextField!
@@ -25,7 +25,7 @@ class LogInViewController: UIViewController {
     @IBOutlet var logInButton: UIButton!
     @IBOutlet var goButton: UIButton!
     
-    
+   
     var currentAction = ActionType.Loaded {
         
         didSet {
@@ -37,14 +37,14 @@ class LogInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         currentAction = .Loaded
         
     }
     
     //MARK: - FUNCTIONS
-
+    
     
     
     @IBAction func signUpButtonTapped(_ sender: Any) {
@@ -61,31 +61,31 @@ class LogInViewController: UIViewController {
     
     
     func updateUI()  {
-         switch currentAction {
-               case .Loaded:
-                headingDetaiLable.text = "Select"
-                emailField.alpha = 0
-                passwordField.alpha = 0
-                logInButton.alpha = 1
-                signUpButton.alpha = 1
-                goButton.alpha = 0
-               case .SignUp:
-                headingDetaiLable.text = "Sign Up"
-                emailField.alpha = 1
-                passwordField.alpha = 1
-                logInButton.alpha = 0
-                signUpButton.alpha = 0
-                goButton.alpha = 1
-               case .EmailLogIn:
-                headingDetaiLable.text = "Log In"
-                emailField.alpha = 1
-                passwordField.alpha = 1
-                logInButton.alpha = 0
-                signUpButton.alpha = 0
-                goButton.alpha = 1
-                 
-                   
-               }
+        switch currentAction {
+        case .Loaded:
+            headingDetaiLable.text = "Select"
+            emailField.alpha = 0
+            passwordField.alpha = 0
+            logInButton.alpha = 1
+            signUpButton.alpha = 1
+            goButton.alpha = 0
+        case .SignUp:
+            headingDetaiLable.text = "Sign Up"
+            emailField.alpha = 1
+            passwordField.alpha = 1
+            logInButton.alpha = 0
+            signUpButton.alpha = 0
+            goButton.alpha = 1
+        case .EmailLogIn:
+            headingDetaiLable.text = "Log In"
+            emailField.alpha = 1
+            passwordField.alpha = 1
+            logInButton.alpha = 0
+            signUpButton.alpha = 0
+            goButton.alpha = 1
+            
+            
+        }
     }
     
     
@@ -104,14 +104,18 @@ class LogInViewController: UIViewController {
             if let email = emailField.text,  let password = passwordField.text  {
                 
                 emailSignUp(email: email, password: password)
+                //self.performSegue(withIdentifier: "toHome", sender: self)
+                self.navigationController?.popViewController(animated: true)
             }
         case .EmailLogIn:
-        
+            
             if let email = emailField.text,  let password = passwordField.text  {
                 
                 emailLogIn(email: email, password: password)
+                //self.performSegue(withIdentifier: "toHome", sender: self)
+                self.navigationController?.popViewController(animated: true)
             }
-       
+            
             
         default:
             break
@@ -119,35 +123,58 @@ class LogInViewController: UIViewController {
     }
     
     
-    
-    
-    
-    
-    
-    
     func emailSignUp(email : String , password : String)  {
-        Auth.auth().createUser(withEmail: email, password: password) { (success, error) in
-            if error != nil {
-                print("LoginVc - New User Created Succesfully")
-            } else{
-                print("LoginVC - Error New User Not Created")
+        
+        let count = email.count
+        if email.contains("@") &&  count>6 {
+            Auth.auth().createUser(withEmail: email, password: password) { (success, error) in
+                if error != nil {
+                    print("LoginVc - New User Not Created ")
+                } else{
+                    print("LoginVC -  New User Created")
+                }
             }
+        } else{
+            let alert = UIAlertController(title: "Alert", message: "Email Syntax Not Correct", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            
         }
     }
+    
+    
+    
+    
+    
+    
     
     func emailLogIn(email : String , password : String)  {
-        Auth.auth().signIn(withEmail: email, password: password) { (success, error) in
-            if error != nil {
-                 print("LoginVc - User Logged In Successfully")
-            }else{
-                print("LoginVC - User Not Logged In")
+        
+        let count = email.count
+        if email.contains("@") &&  count>6 {
+            
+            Auth.auth().signIn(withEmail: email, password: password) { (success, error) in
+                if error != nil {
+                    print("LoginVc - User Not Logged In")
+                }else{
+                    print("LoginVC - User Logged In")
+                
+                }
             }
+            
+        } else {
+            let alert = UIAlertController(title: "Alert", message: "Email Syntax Not Correct", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
         }
+
     }
     
     
     
     
     
-
+    
 }
